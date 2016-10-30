55c63880e0848dd4066fcac5058c3358f6151eff
root <root@fisheah2.miniserver.com>
2016-10-30 21:47:43 +0000

    Initial commit

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'parent-style'; // This is 'twentysixteen-style' for the Twenty Sixteen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+// Our custom post type function
+function create_posttype() {
+    register_post_type( 'movies',
+    // CPT Options
+        array(
+	    'labels' => array(
+	     		'name' => __( 'Movies' ),
+	      	      'singular_name' => __( 'Movie' )
+	      	 ),
+		'public' => true,
+	    'has_archive' => true,
+	    'rewrite' => array('slug' => 'movies'),
+														     	       )
+															       );
+}
+// Hooking up our function to theme setup
+add_action( 'init', 'create_posttype' );
+?>
